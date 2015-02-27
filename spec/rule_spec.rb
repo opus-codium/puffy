bcc6b98b32cbe0afee335b8b273ff2dd8b132395
Romain Tartière <romain@blogreen.org>
2015-02-27 17:10:53 +0100

    Add a bunch of directions.

--- spec/rule_spec.rb
+++ spec/rule_spec.rb
@@ -4,16 +4,16 @@ module Melt
   RSpec.describe Rule do
     it 'detects IPv4 rules' do
       expect(Rule.new.ipv4?).to be_truthy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { port: 80 }).ipv4?).to be_truthy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { host: IPAddress.parse('192.0.2.1'), port: 80 }).ipv4?).to be_truthy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { host: IPAddress.parse('2001:DB8::1'), port: 80 }).ipv4?).to be_falsy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { port: 80 }).ipv4?).to be_truthy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { host: IPAddress.parse('192.0.2.1'), port: 80 }).ipv4?).to be_truthy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { host: IPAddress.parse('2001:DB8::1'), port: 80 }).ipv4?).to be_falsy
     end
 
     it 'detects IPv6 rules' do
       expect(Rule.new.ipv6?).to be_truthy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { port: 80 }).ipv6?).to be_truthy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { host: IPAddress.parse('192.0.2.1'), port: 80 }).ipv6?).to be_falsy
-      expect(Rule.new(action: :block, proto: :tcp, dst: { host: IPAddress.parse('2001:DB8::1'), port: 80 }).ipv6?).to be_truthy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { port: 80 }).ipv6?).to be_truthy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { host: IPAddress.parse('192.0.2.1'), port: 80 }).ipv6?).to be_falsy
+      expect(Rule.new(action: :block, dir: :out, proto: :tcp, dst: { host: IPAddress.parse('2001:DB8::1'), port: 80 }).ipv6?).to be_truthy
     end
   end
 end
