1ca24a15808e96dc51e03d12f7103dbe406f4987
Romain Tartière <romain@blogreen.org>
2015-02-27 15:39:17 +0100

    Drop now useless NilClass extension.

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -1,21 +1,3 @@
-class NilClass # :nodoc:
-  # Allow nil to yield with no value.
-  #
-  #   foo = [1, 2, 3]
-  #   bar = nil
-  #
-  #   foo.each { |x| puts "foo: #{x.inspect}" }
-  #   bar.each { |x| puts "bar: #{x.inspect}" }
-  # <i>produces:</i>
-  #   foo: 1
-  #   foo: 2
-  #   foo: 3
-  #   bar: nil
-  def each
-    yield(nil)
-  end
-end
-
 class Object # :nodoc:
   # Return self packed in an array unless self itself is an Array.
   #
