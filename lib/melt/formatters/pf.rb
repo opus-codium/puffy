6c4e0a5e70282e58ef735e50e3c0790b9fa92864
Romain Tartière <romain@blogreen.org>
2015-03-16 11:22:20 +0100

    Build complete rulesets.

--- lib/melt/formatters/pf.rb
+++ lib/melt/formatters/pf.rb
@@ -11,6 +11,7 @@ module Melt
         end
         parts << rule.dir if rule.dir
         parts << 'quick' unless rule.no_quick
+        parts << "on #{rule.on.gsub('!', '! ')}" if rule.on
         parts << rule.af if rule.af
         parts << "proto #{rule.proto}" if rule.proto
         if rule.from && (rule.from[:host] || rule.from[:port]) then
@@ -23,7 +24,31 @@ module Melt
           parts << emit_address(rule.to[:host]) if rule.to[:host]
           parts << "port #{rule.dst_port}" if rule.dst_port
         end
+        if rule.rdr_to && rule.rdr_to[:host] then
+          parts << "rdr-to #{emit_address(rule.rdr_to[:host])}"
+          parts << "port #{rule.rdr_to_port}" if rule.rdr_to_port
+        end
+        if rule.nat_to then
+          parts << "nat-to #{emit_address(rule.nat_to)}"
+        end
         parts.join(' ')
+      end
+
+      # Returns a Pf String representation of the provided +rules+ Array of Rule.
+      def emit_ruleset(rules, policy = :block)
+        parts = []
+
+        parts << 'match in all scrub (no-df)'
+        parts << 'set skip on lo'
+        parts << super([Rule.new(action: policy, return: true, no_quick: true)])
+
+        parts << super([Rule.new(action: :block, return: true, dir: :in, on: '!lo0', proto: :tcp, to: { port: '6000:6010' }, no_quick: true)])
+
+        parts << super(rules.select { |r| r.nat? })
+        parts << super(rules.select { |r| r.rdr? })
+        parts << super(rules.select { |r| r.filter? })
+
+        parts.reject { |s| s.empty? }.join("\n")
       end
 
     protected
