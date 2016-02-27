8210e448cbd48838bf4ba7d058cfc4e946097c79
Romain Tartière <romain@blogreen.org>
2016-02-27 19:41:23 +0100

    Run all cucumber tests in-process.

--- features/puppet.feature
+++ features/puppet.feature
@@ -1,5 +1,4 @@
 Feature: Puppet
-  @in-process
   Scenario: Generate firewall rules for a host
     Given a file named "network.rb" with:
     """
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,10 +5,10 @@ require 'melt/cli'
 class Runner
   def initialize(argv, stdin, stdout, stderr, kernel)
     @argv   = argv
-    @stdin  = stdin
-    @stdout = stdout
-    @stderr = stderr
-    @kernel = kernel
+    $stdin  = stdin
+    $stdout = stdout
+    $stderr = stderr
+    $kernel = kernel
   end
 
   def execute!
@@ -16,11 +16,5 @@ class Runner
   end
 end
 
-Before('@in-process') do
-  aruba.config.command_launcher = :in_process
-  aruba.config.main_class = Runner
-end
-
-After('@in-process') do
-  aruba.config.command_launcher = :spawn
-end
+Aruba.config.command_launcher = :in_process
+Aruba.config.main_class = Runner
