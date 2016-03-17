d9eeedcf5a677ce5b82b30e314e0df597b1eb800
Romain Tartière <romain@blogreen.org>
2016-03-17 18:18:07 +0100

    Remove Object#to_array now unused.

--- lib/core_ext.rb
+++ lib/core_ext.rb
@@ -46,18 +46,3 @@ end
 class Hash # :nodoc:
   include Expandable
 end
-
-class Object # :nodoc:
-  # Return self packed in an array unless self itself is an Array.
-  #
-  #   1.to_array         #=> [1]
-  #   'string'.to_array  #=> ['string']
-  #   [1, 2, 3].to_array #=> [1, 2, 3]
-  def to_array
-    if is_a?(Array)
-      self
-    else
-      [self]
-    end
-  end
-end
